74d2ac7267516fee26e95f432f9c61d7f15c4bbd
razisek <agusdono27@gmail.com>
2024-11-12 14:24:12 +0700

    Add Swagger Docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
-{
-    //
-}
+/**
+ * @OA\Info(
+ *    title="Clinic API Documentation",
+ *    version="1.0.0",
+ *    description="API Documentation for Clinic Appointment System"
+ * )
+ */
+abstract class Controller {}
