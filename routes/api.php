da54f295d48d7148cca805fc3c2aebd3e8515599
razisek <agusdono27@gmail.com>
2024-11-12 14:08:43 +0700

    Add Route api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AppointmentController;
+use App\Http\Controllers\Api\DiagnoseController;
+use App\Http\Controllers\Api\PatientController;
+use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/patient', [PatientController::class, 'store']);
+Route::post('/diagnose', [DiagnoseController::class, 'store']);
+Route::post('/service', [ServiceController::class, 'store']);
+
+Route::post('/appointment', [AppointmentController::class, 'store']);
+Route::get('/appointment/{appointment}', [AppointmentController::class, 'show']);
+Route::patch('/appointment/{appointment}', [AppointmentController::class, 'update']);
